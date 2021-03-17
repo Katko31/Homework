97269775f167cdd03cb52c630e9e62bee0746ec9
kate <kate.smirnova.2016@gmail.com>
2021-03-17 22:14:11 +0300

    data_str4

--- Pr/Data_list.py
+++ Pr/Data_list.py
@@ -4,7 +4,7 @@ class Box:
 
     def __init__(self, data):
         self.data = data
-        self.link = None    # ссылка, которая смотрить на предыдущий элемент
+        self.link = None    # ссылка, которая в конструкторе коробки всегда пустая
 
 
 class MyList:
@@ -41,17 +41,64 @@ class MyList:
             end = end.link
         end.link = None
 
+    def insert(self, new_data, position):
+        new_element = Box(new_data)
+        if self.pointer is None:
+            self.pointer = new_element
+            return
+
+        mid1 = self.pointer
+        i = 0
+        while mid1.link:
+            if i != position:
+                mid1 = mid1.link
+                i += 1
+
+            new_element.link = mid1.link
+            mid2 = self.pointer
+            i = 0
+            while mid2.link:
+                if i != position - 1:
+                    mid2 = mid2.link
+                    i += 1
+                mid2.link = new_element
+                break
+            break
+
+    def get_data_value(self, data):
+        if self.pointer is None:
+            raise ValueError('Список пуст')
+        finder = self.pointer
+        i = 0
+        while finder.link:
+            if finder.data != data:
+                finder = finder.link
+                i += 1
+            else:
+                return f'{data}[{i}]'
+
+    def get_index_value(self, index):
+        if self.pointer is None:
+            raise ValueError('Список пуст')
+        finder = self.pointer
+        i = 0
+        while finder.link:
+            if i != index:
+                finder = finder.link
+                i += 1
+            else:
+                return f'{finder.data}[{i}]'
+
+        return f'{finder.data}[{i}]'
+
+    def __getitem__(self, item):
+        return self.pointer.data[item]
 
 
-
-
-
-a = MyList()
-
-
-a.append_start(12)
-a.append_start(11)
-a.append_start(15)
-a.append_start(19)
-
-print(a)
+if __name__ == "__main__":
+    a = MyList()
+    a.append_start(12)
+    a.append_start(11)
+    a.append_start(15)
+    a.append_start(19)
+    print(a.get_index_value(3))
